bca93667635d3a9b043e5a6ace54bb0cbab96a4c
Bibo Hao <haobibo@users.noreply.github.com>
2024-03-13 20:04:47 +0800

    fix startup script

--- docker_dev_box/work/start-jupyterhub.sh
+++ docker_dev_box/work/start-jupyterhub.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 [ $BASH ] && [ -f /etc/profile  ] && [ -z $ENTER_PROFILE ] && . /etc/profile
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
--- docker_dev_box/work/start-singleuser.sh
+++ docker_dev_box/work/start-singleuser.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+[ $BASH ] && [ -f /etc/profile  ] && [ -z $ENTER_PROFILE ] && . /etc/profile
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/start--pre.sh
 
