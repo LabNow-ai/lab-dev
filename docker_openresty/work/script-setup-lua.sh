9b0570cb661e9ec1e74c54061a2f14a126cbdae0
Bibo Hao <haobibo@users.noreply.github.com>
2024-03-26 18:30:57 +0000

    debug oprenresty

--- docker_openresty/work/script-setup-lua.sh
+++ docker_openresty/work/script-setup-lua.sh
@@ -1,14 +1,16 @@
 source /opt/utils/script-utils.sh
 
+export LUA_HOME=/opt/lua
+
 setup_lua_base() {
     VERSION_LUA=$(curl -sL https://www.lua.org/download.html | grep "cd lua" | head -1 | grep -Po '(\d[\d|.]+)') \
  && URL_LUA="http://www.lua.org/ftp/lua-${VERSION_LUA}.tar.gz" \
  && echo "Downloading LUA ${VERSION_LUA} from ${URL_LUA}" \
  && install_tar_gz $URL_LUA \
  && mv /opt/lua-* /tmp/lua && cd /tmp/lua \
- && make linux test && make install INSTALL_TOP=/opt/lua \
+ && make linux test && make install INSTALL_TOP=${LUA_HOME} \
+ && ln -sf ${LUA_HOME}/bin/lua* /usr/bin/ \
  && rm -rf /tmp/lua \
- && ln -sf /opt/lua/bin/lua* /usr/bin/ \
  && echo "@ Version of LUA installed: $(lua -v)"
 }
 
@@ -19,8 +21,8 @@ setup_lua_rocks() {
  && echo "Downloading luarocks ${VERSION_LUA_ROCKS} from ${URL_LUA_ROCKS}" \
  && install_tar_gz $URL_LUA_ROCKS \
  && mv /opt/luarocks-* /tmp/luarocks && cd /tmp/luarocks \
- && ./configure --prefix=/opt/lua && make install \
- && rm -rf /tmp/luarocks \
+ && ./configure --prefix=${LUA_HOME} --with-lua-include=${LUA_HOME}/include && make install \
  && ln -sf /opt/lua/bin/lua* /usr/bin/ \
- && echo "@ Version of luarocks: $(luarocks -version)"
+ && rm -rf /tmp/luarocks \
+ && echo "@ Version of luarocks: $(luarocks --version)"
 }
