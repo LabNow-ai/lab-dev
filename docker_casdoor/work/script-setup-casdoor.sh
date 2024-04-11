3913e20a9fee34f875157a62a542910a74c83f51
Bibo Hao <haobibo@users.noreply.github.com>
2024-04-11 16:50:19 +0000

    debug casdoor build

--- docker_casdoor/work/script-setup-casdoor.sh
+++ docker_casdoor/work/script-setup-casdoor.sh
@@ -3,11 +3,12 @@ source /opt/utils/script-utils.sh
 setup_casdoor() {
   # Install the latest release of casdoor
      VER_CASDOOR=$(curl -sL https://github.com/casdoor/casdoor/releases.atom | grep 'releases/tag' | head -1 | grep -Po '\d[\d.]+' ) \
-  && URL_CASDOOR="https://github.com/casdoor/casdoor/archive/refs/tags/v${VER_CASDOOR}.tar.gz" \  
+  && URL_CASDOOR="https://github.com/casdoor/casdoor/archive/refs/tags/v${VER_CASDOOR}.tar.gz" \
   && echo "Downloading casdoor version ${VER_CASDOOR} from: ${URL_CASDOOR}" \
   && install_tar_gz $URL_CASDOOR \
   && mv /opt/casdoor-* /tmp/casdoor && mkdir -pv /opt/casdoor/web /opt/casdoor/conf \
   && echo "Building Frontend..." \
+  && cd /tmp && corepack enable && yarn -v \
   && cd /tmp/casdoor/web \
   && yarn install --frozen-lockfile --network-timeout 1000000 && yarn run build \
   && mv ./build /opt/cadoor/web/ \
