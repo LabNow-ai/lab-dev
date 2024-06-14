f8c594aae1236cf9172fec593495ec206ab775ef
Bibo Hao <haobibo@users.noreply.github.com>
2024-06-14 14:49:49 +0000

    fix jupyter kernels

--- docker_dev_box/work/script-devbox-jupyter.sh
+++ docker_dev_box/work/script-devbox-jupyter.sh
@@ -11,7 +11,7 @@ setup_jupyter_base() {
 
 
 setup_jupyter_kernels() {
-  echo "@ Jupyter Kernels RefList: https://github.com/jupyter/jupyter/wiki/Jupyter-kernels"
+     echo "@ Jupyter Kernels RefList: https://github.com/jupyter/jupyter/wiki/Jupyter-kernels"
 
      echo "@ Install Jupyter Kernel for Bash" \
   && pip install -Uq bash_kernel && python -m bash_kernel.install --sys-prefix
@@ -44,24 +44,32 @@ setup_jupyter_kernels() {
   && $GOPATH/bin/gonb --install \
   && mv ~/.local/share/jupyter/kernels/gonb /opt/conda/share/jupyter/kernels/
 
-  ## TODO: buggy, cargo install sccache
+  ## checked @ 2024-0614
      which rustc \
-  && echo "@ Install Jupyter Kernel for Rust: https://github.com/evcxr/evcxr/issues/140" \
-  && cargo install --locked evcxr_jupyter \
-  && evcxr_jupyter --install --sys-prefix \
+  && echo "@ Install Jupyter Kernel for Rust: https://github.com/evcxr/evcxr/blob/main/evcxr_jupyter/README.md" \
+  # alternative approaches to install evxcr_jupyter:
+  # && cargo install --locked evcxr_jupyter \
+  && VER_EVCXR=$(curl -sL https://github.com/evcxr/evcxr/releases.atom | grep 'releases/tag' | head -1 | grep -Po '\d[\d.]+' ) \
+  && URL_EVCXR="https://github.com/evcxr/evcxr/releases/download/v${VER_EVCXR}/evcxr_jupyter-v${VER_EVCXR}-x86_64-unknown-linux-gnu.tar.gz" \
+  && echo "Downloading evcxr version ${VER_EVCXR} from: ${URL_EVCXR}" \
+  && install_tar_gz $URL_EVCXR && mv /opt/evcxr* /tmp/evcxr && mv /tmp/evcxr/evcxr_jupyter /opt/cargo/bin/ \
+  && /opt/cargo/bin/evcxr_jupyter --install --sys-prefix \
   && mv ~/.local/share/jupyter/kernels/rust /opt/conda/share/jupyter/kernels/
-
-  ## TODO: buggy
-     which java \
-  && pip install -Uq pandas py4j
-  #&& pip install beakerx && beakerx install \
-  #&& jupyter labextension install beakerx-jupyterlab
 
   ## checked @ 2024-0307
      which julia \
   && echo "@ Install Jupyter Kernel for Julia: https://github.com/JuliaLang/IJulia.jl" \
   && julia -e "using Pkg; Pkg.add(\"IJulia\"); Pkg.precompile();" \
   && mv ~/.local/share/jupyter/kernels/julia* /opt/conda/share/jupyter/kernels/
+
+  ## Checked @ 2024-0614
+     which java \
+  && export JBANG_DIR=/opt/jbang \
+  && echo "export JBANG_DIR=${JBANG_DIR}" > /etc/profile.d/path-jbang.sh \
+  && curl -Ls https://sh.jbang.dev | bash -s - app setup \
+  && jbang trust add https://github.com/jupyter-java \
+  && jbang install-kernel@jupyter-java \
+  && mv ~/.local/share/jupyter/kernels/jbang-* /opt/conda/share/jupyter/kernels/
 
   ## https://github.com/jupyter-xeus/xeus-octave  # TODO: to check
      which octave \
