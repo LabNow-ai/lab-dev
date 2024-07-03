138bef96f8839c0aa79bd2f68c20cc275eaed555
Bibo Hao <haobibo@users.noreply.github.com>
2024-07-03 14:42:36 +0000

    update vscode server install

--- docker_dev_box/work/script-devbox-vscode.sh
+++ docker_dev_box/work/script-devbox-vscode.sh
@@ -1,13 +1,32 @@
 source /opt/utils/script-utils.sh
 
-# ref: https://github.com/coder/code-server
 
 setup_vscode_base() {
+  ## ref: https://github.com/coder/code-server
      VERSION_CODER=$(curl -sL https://github.com/cdr/code-server/releases.atom | grep "releases/tag" | head -1 | grep -Po '(\d[\d|.]+)') \
   && install_tar_gz "https://github.com/cdr/code-server/releases/download/v${VERSION_CODER}/code-server-${VERSION_CODER}-linux-amd64.tar.gz" \
   && mv /opt/code-server* /opt/code-server \
   && ln -s /opt/code-server/bin/code-server /usr/bin/ \
-  && printf "#!/bin/bash\n/opt/code-server/bin/code-server --port=8888 --auth=none --disable-telemetry ${HOME}\n" > /usr/local/bin/start-code-server.sh \
-  && chmod u+x /usr/local/bin/start-code-server.sh \
-  && echo "@ coder-server Version: $(code-server -v)"
+  && printf "#! /usr/bin/env bash\n/opt/code-server/bin/code-server --port=8888 --auth=none --disable-telemetry ${HOME}\n" > /usr/local/bin/start-code-server.sh \
+  && chmod u+x /usr/local/bin/start-code-server.sh
+
+  type code-server && echo "@ Version of coder-server: $(code-server -v)" || return -1;
+}
+
+
+setup_vscode_base2() {
+  ## ref: https://github.com/gitpod-io/openvscode-server
+     ARCH="x64"
+     VER_CODE_SERVER=$(curl -sL https://github.com/gitpod-io/openvscode-server/releases.atom | grep "releases/tag" | grep -v 'inside' | head -1 | grep -Po '(\d[\d|.]+)') \
+  && URL_CODE_SERVER="https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v${VER_CODE_SERVER}/openvscode-server-v${VER_CODE_SERVER}-linux-${ARCH}.tar.gz" \
+  && echo "Downloading gitpod-io/openvscode-server version ${VER_CODE_SERVER} from: ${URL_CODE_SERVER}" \
+  && install_tar_gz $URL_CODE_SERVER \
+  && mv /opt/openvscode-server* /opt/code-server \
+  && ( which node && ( echo "Replacing with system node" && ln -sf $(which node) /opt/code-server/ ) || echo "No system node found" ) \
+  && ln -sf /opt/code-server/bin/openvscode-server /opt/code-server/bin/code-server \
+  && ln -sf /opt/code-server/bin/openvscode-server /usr/bin/code-server \
+  && printf "#! /usr/bin/env bash\n/opt/code-server/bin/code-server --host=0.0.0.0 --port=8888 --telemetry-level=off \"${HOME:-~}\"\n" > /usr/local/bin/start-code-server.sh \
+  && chmod u+x /usr/local/bin/start-code-server.sh
+
+  type code-server && echo "@ Version of openvscoder-server: $(code-server -v)" || return -1;  
 }
