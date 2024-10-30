46f51a3cc8470fd908fe2c70ab529c732a1ae672
Bibo Hao <haobibo@users.noreply.github.com>
2024-10-30 04:06:20 +0000

    update vscoder script

--- docker_dev_box/work/start-code-server.sh
+++ docker_dev_box/work/start-code-server.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
+[ $BASH ] && [ -f /etc/profile  ] && [ -z $ENTER_PROFILE ] && . /etc/profile
 
-/opt/code-server/bin/code-server --port=9999 --auth=none --disable-telemetry /root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+
+if [[ "$CODER_ARGS $@" != *"--bind-addr="* ]]; then
+  CODER_ARGS="--bind-addr=0.0.0.0:9999 $CODER_ARGS"
+fi
+
+
+exec /opt/code-server/bin/code-server --auth=none --disable-telemetry --disable-update-check ${CODER_ARGS} $@ /root
