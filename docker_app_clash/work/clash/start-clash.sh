0df2c76968e5c27bb1ed795ff2d46f6f686ab774
Bibo Hao <haobibo@users.noreply.github.com>
2024-11-22 06:33:29 +0000

    debug clash start script

--- docker_app_clash/work/clash/start-clash.sh
+++ docker_app_clash/work/clash/start-clash.sh
@@ -4,6 +4,6 @@ set -eux
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Setting PROXY_PROVIDER to: ${PROXY_PROVIDER}!"
-sed -i -e "s/PROXY_PROVIDER/${PROXY_PROVIDER}/g" ${CLASH_CONFIG_PATH:-"/opt/clash/config/config.yml"}
+sed -i -e "s|PROXY_PROVIDER|${PROXY_PROVIDER}|g" "${CLASH_CONFIG_PATH:-"/opt/clash/config/config.yaml"}"
 
 exec /opt/clash/clash -d config $@
