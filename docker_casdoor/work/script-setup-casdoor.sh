6b7303c8ddc967c8f0d5d69cf127a9df97251866
Bibo Hao <haobibo@users.noreply.github.com>
2026-02-17 15:39:08 +0800

    debug casdoor fed build (#49)
    * debug casdoor fed build
    
    * debug casdoor build
    
    * debug
    
    * update nginx config
    
    * check casdoor build

--- docker_casdoor/work/script-setup-casdoor.sh
+++ docker_casdoor/work/script-setup-casdoor.sh
@@ -17,16 +17,18 @@ setup_casdoor() {
   && ./build.sh \
   && mv "./server_linux_${ARCH}" ./swagger ./docker-entrypoint.sh ./version_info.txt /opt/casdoor/ \
   && cat ./conf/app.conf | sort > /opt/casdoor/conf/app.conf \
-  && ln -sf "/opt/casdoor/server_linux_${ARCH}" /opt/casdoor/server
-  # && go test -v -run TestGetVersionInfo ./util/system_test.go ./util/system.go ./util/variable.go \
+  && ln -sf "/opt/casdoor/server_linux_${ARCH}" /opt/casdoor/server ;
+  # && go test -v -run TestGetVersionInfo ./util/system_test.go ./util/system.go ./util/variable.go
+  [ -f "/opt/casdoor/server_linux_${ARCH}" ] && echo "Successfully built casdoor backend!" || exit 1 ;
 
      echo "--> Building Frontend..." \
   && cd /tmp && npm install -g yarn && yarn -v \
   && cd /tmp/casdoor/web \
-  && yarn set version berry \
-  && yarn install --frozen-lockfile --network-timeout 1000000 \
-  && NODE_OPTIONS="--max-old-space-size=4096" yarn run build \
-  && mv ./build*/* /opt/casdoor/web/build/
+  && export NODE_OPTIONS="--max-old-space-size=4096" && export GENERATE_SOURCEMAP=false \
+  && jq 'del(.scripts.preinstall)' package.json > package.tmp.json && mv package.tmp.json package.json \
+  && yarn install --frozen-lockfile --network-timeout 1000000 && yarn run build \
+  && mv ./build*/* /opt/casdoor/web/build/ ;
+  [ -f "/opt/casdoor/web/build/index.html" ] && echo "Successfully built casdoor frontend!" || exit 2 ;
 
      echo "--> Finished building casdoor to /opt/casdoor!" \
   && rm -rf /tmp/casdoor \
