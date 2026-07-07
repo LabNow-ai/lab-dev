d8e802f477df0c492f32029ce028b790cbf3a719
Bibo Hao <haobibo@users.noreply.github.com>
2026-07-07 16:20:38 +0800

    debug claw config

--- docker_openclaw/work/start-openclaw.sh
+++ docker_openclaw/work/start-openclaw.sh
@@ -62,10 +62,7 @@ update_config() {
         | if $use_proxy == "true" then
             .gateway.auth.mode = "trusted-proxy"
             | .gateway.auth.trustedProxy.userHeader = "x-auth-request-user"
-            | .gateway.auth.trustedProxy.requiredHeaders = [
-                "x-forwarded-proto",
-                "x-forwarded-host"
-              ]
+            | .gateway.auth.trustedProxy.requiredHeaders = []
             | .gateway.auth.trustedProxy.allowLoopback = true
             | del(.gateway.auth.token, .gateway.auth.password)
           else
