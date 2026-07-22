2c72e46981e433fe9135a86315ca15d2520faf5c
Bibo Hao <haobibo@users.noreply.github.com>
2026-07-23 01:24:27 +0800

    update nocobase

--- docker_nocobase/work/script-setup-nocobase.sh
+++ docker_nocobase/work/script-setup-nocobase.sh
@@ -1,4 +1,5 @@
 setup_nocobase_create_app(){
+ ## ref: https://github.com/nocobase/nocobase/blob/main/docker/nocobase/Dockerfile-full
     local VER_NOCO=${1:-"latest"} \
  && npx -y create-nocobase-app@${VER_NOCO} nocobase --empty-key --skip-dev-dependencies -a -e APP_ENV=production \
  && cd nocobase \
@@ -6,7 +7,10 @@ setup_nocobase_create_app(){
  && rm -rf yarn.lock \
  && find node_modules -type f -name "yarn.lock"     -delete \
  && find node_modules -type f -name "bower.json"    -delete \
- && find node_modules -type f -name "composer.json" -delete ;
+ && find node_modules -type f -name "composer.json" -delete \
+ && find node_modules -type d -name docs ! -path "node_modules/@nocobase/*" -prune -exec rm -rf '{}' + \
+ && find node_modules -type f -name "*.map" -delete \
+ && find node_modules -type f -name "*.md" ! -path "node_modules/@nocobase/*" -delete ;
 }
 
 setup_nocobase_from_source(){
